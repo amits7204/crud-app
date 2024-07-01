7a638ec43f3360366b5d277180ba49fa1b36bf09
Amit Singh <amit.singh@dwatatech.com>
2024-07-02 00:24:48 +0530

    feat: create table insert the data

--- index.ts
+++ index.ts
@@ -1,10 +1,30 @@
 import mongoose from "mongoose";
+import Product from "./models/product.model";
+
 const express = require("express");
+
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (req: any, res: { send: (arg0: string) => void }) => {
     res.send("Hello from Node API 3000 amit");
 });
+
+app.post(
+    "/api/produucts",
+    async (
+        req: any,
+        res: { send: (arg0: string) => void; status: (arg0: Number) => any }
+    ) => {
+        try {
+            const product = await Product.create(req.body);
+            res.status(200).json(product);
+        } catch (error: any) {
+            res.status(500).json({ message: error.message });
+        }
+    }
+);
 
 mongoose
     .connect(
---
+++ models/product.model.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const ProductSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            require: [true, "Please enter product name"],
+        },
+        quantity: {
+            type: Number,
+            require: true,
+            default: 0,
+        },
+        price: {
+            type: Number,
+            require: true,
+            default: 0,
+        },
+        image: {
+            type: String,
+            require: false,
+        },
+    },
+    { timestamps: true }
+);
+
+const Product = mongoose.model("Product", ProductSchema);
+export default Product;
