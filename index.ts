3085128857beb1422803edb69010e440f72f18a3
Amit Singh <amit.singh@dwatatech.com>
2024-07-02 23:57:05 +0530

    add delete and update api

--- index.ts
+++ index.ts
@@ -6,13 +6,33 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req: any, res: { send: (arg0: string) => void }) => {
     res.send("Hello from Node API 3000 amit");
 });
 
+app.get("/api/products", async (req: any, res: any) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products);
+    } catch (error: any) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+app.get("/api/product/:id", async (req: any, res: any) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    } catch (error: any) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
 app.post(
-    "/api/produucts",
+    "/api/products",
     async (
         req: any,
         res: { send: (arg0: string) => void; status: (arg0: Number) => any }
@@ -25,6 +45,41 @@ app.post(
         }
     }
 );
+
+/**
+ * update product by id
+ */
+
+app.put("/api/product/:id", async (req: any, res: any) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body);
+        if (!product) {
+            return res.status(404).json({ mesage: "Product not found" });
+        }
+        const updatedProduct = await Product.findById(id);
+        res.status(200).json(updatedProduct);
+    } catch (error: any) {
+        res.status(500).json({ message: error?.message });
+    }
+});
+
+/**
+ *  delete product by id
+ */
+
+app.delete("/api/product/:id", async (req: any, res: any) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findByIdAndDelete(id);
+        if (!product) {
+            return res.status(404).json({ mesage: "Product not found" });
+        }
+        res.status(200).json({ message: "Product deleted successfully" });
+    } catch (error: any) {
+        res.status(500).json({ message: error.message });
+    }
+});
 
 mongoose
     .connect(
